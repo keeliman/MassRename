921b3579028b45c03981f8d568a122919478b248
Jerome B. <77253921+keeliman@users.noreply.github.com>
2023-10-18 11:43:49 +0200

    Optimize

--- mass_rename.py
+++ mass_rename.py
@@ -2,7 +2,7 @@ import os
 import re
 import configparser
 
-# Charger les configurations depuis le fichier .ini
+# Load configurations from the .ini file
 config = configparser.ConfigParser()
 config.read('config.ini')
 
@@ -10,28 +10,26 @@ try:
     DIRECTORY_PATH = config['DEFAULT']['DIRECTORY_PATH']
     LAST_PUBLISHED = int(config['DEFAULT']['LAST_PUBLISHED'])
 except KeyError:
-    print("Erreur : Une clé manquante dans le fichier de configuration.")
+    print("Error: Missing key in the configuration file.")
     exit()
 
 def is_fully_numeric(filename):
-    """Vérifie si le nom du fichier (sans extension) est entièrement numérique."""
+    """Check if the filename (without extension) is fully numeric."""
     name, _ = os.path.splitext(filename)
     return name.isdigit()
 
 def extract_numeric(filename):
-    """Extrait le numéro du nom du fichier."""
+    """Extract the number from the filename."""
     name, _ = os.path.splitext(filename)
     numbers = re.findall(r'\d+', name)
-    if numbers:
-        return int(numbers[0])
-    return None
+    return int(numbers[0]) if numbers else None
 
 def rename_files_in_directory(directory, last_published):
-    """Renomme en masse les fichiers dans le répertoire donné."""
-    # Liste des fichiers dans le répertoire
+    """Bulk rename files in the given directory."""
+    # List of files in the directory
     files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
     
-    # Vérification que tous les fichiers sont numériques
+    # Ensure all filenames are numeric
     for filename in files:
         if not is_fully_numeric(filename):
             num = extract_numeric(filename)
@@ -39,30 +37,29 @@ def rename_files_in_directory(directory, last_published):
                 new_name = str(num) + os.path.splitext(filename)[1]
                 os.rename(os.path.join(directory, filename), os.path.join(directory, new_name))
             else:
-                print(f"Erreur : Impossible de parser le numéro du fichier {filename}")
+                print(f"Error: Unable to parse the file number from {filename}")
                 return
 
-    # Vérification que les vidéos sont bien numérotées à partir de 1
+    # Ensure videos are numbered starting from 1
     numbers = sorted([int(os.path.splitext(f)[0]) for f in files])
     if numbers[0] != 1:
-        print("Erreur : Les vidéos ne sont pas numérotées à partir de 1.")
+        print("Error: Videos are not numbered starting from 1.")
         return
 
-    # Renommer les fichiers en incrémentant par le dernier numéro publié
+    # Rename files by incrementing with the last published number
     for filename in files:
         num = int(os.path.splitext(filename)[0])
         new_name = str(num + last_published) + os.path.splitext(filename)[1]
         os.rename(os.path.join(directory, filename), os.path.join(directory, new_name))
 
-    print("Renommage terminé avec succès!")
-
+    print("Renaming completed successfully!")
 
 if __name__ == "__main__":
     if not os.path.exists(DIRECTORY_PATH):
-        print(f"Erreur : Le répertoire {DIRECTORY_PATH} n'existe pas.")
+        print(f"Error: The directory {DIRECTORY_PATH} does not exist.")
         exit()
 
     try:
         rename_files_in_directory(DIRECTORY_PATH, LAST_PUBLISHED)
     except Exception as e:
-        print(f"Une erreur s'est produite : {e}")
+        print(f"An error occurred: {e}")
